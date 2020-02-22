9faae9802f2a4d9ca11f1c8173d502b46ae8ec29
kovi <kovihome86@gmail.com>
2020-02-22 21:17:26 +0100

    Redmine: #500 - photometry rewrite in python

---
+++ pmutil/src/main/python/pmbase.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+#
+# PmUtils/pmrefcat
+#
+'''
+Created on Jan 1, 2020
+
+@author: kovi
+'''
+
+from sys import argv
+from getopt import getopt, GetoptError
+from urllib import request
+from glob import glob
+import json
+import xmltodict
+import re
+import subprocess
+import os
+
+Color_Off = '\033[0m'  # Text Reset
+BRed = "\033[1;31m"  # Red
+BGreen = '\033[1;32m'  # Green
+Blue = '\033[0;34m'  # Blue
+BCyan = '\033[1;36m'  # Cyan
+
+
+def printError(s):
+    print(BRed + "Error: " + s + Color_Off)
+
+
+def printWarning(s):
+    print(BGreen + "Warning: " + s + Color_Off)
+
+
+def printInfo(s):
+    print(BCyan + s + Color_Off)
+
+
+def loadPplSetup():
+    pplSetup = {}
+    userhome = os.getenv("HOME")
+    f = open(userhome + "/bin/ppl-setup")
+    pplSetup['HOME'] = userhome
+    for line in f:
+        if not line.startswith('#') and line.strip() != "":
+            r = line.split('=')
+            value = r[1].rstrip()[1:-1]
+            if '$' in value:
+                for k in pplSetup.keys():
+                    if '$' + k in value:
+                        value = value.replace('$' + k, pplSetup[k])
+            pplSetup[r[0].strip()] = value
+    return pplSetup
+
+
+def invoke(cmd):
+    r = cmd.split()
+    a = subprocess.check_output(r)
+    return a.decode('ascii')[:-1]
+
+
+def hexa2deg(s):
+    r = s.split(':')
+    sign = 1.0
+    if r[0].startswith('-'):
+        sign = -1.0
+        r[0] = r[0][1:]
+    d = float(r[0])
+    m = float(r[1])
+    s = float(r[2])
+    return sign * (d + m / 60.0 + s / 3600.0)
+
+
+def deg2hexa(d):
+    sign = ''
+    if d < 0.0:
+        d = -d
+        sign = '-'
+    dd = int(d)
+    r = (d - float(dd)) * 60.0
+    mm = int(r)
+    ss = (r - float(mm)) * 60.0
+    return "%s%02d:%02d:%04.1f" % (sign, dd, mm, ss)
+
+
+def jd(dateObs):
+    # d = datetime.fromisoformat(dateObs) # python 3.7+
+    dt = dateObs.split('T')
+    dd = dt[0].split('-')
+    tt = dt[1].split(':')
+    d = datetime(int(dd[0]), int(dd[1]), int(dd[2]), int(tt[0]), int(tt[1]), int(tt[2]))
+
+    if d.month == 1 or d.month == 2:
+        d.year -= 1
+        d.month += 12
+    if d.month > 2:
+        year = d.year
+        month = d.month
+    if year >= 1582:
+        a = int(year / 100)
+        b = 2 - a + int(a / 4)
+    else:
+        b = 0
+
+    day = d.day + d.hour / 24 + d.minute / (24 * 60) + d.second / (24 * 3600)
+    jd = int(365.25 * (year + 4716)) + int(30.6001 * (month + 1)) + day + b - 1524.5
+    return jd
+
+def getPair(s, delim = ','):
+    ss = s.split(delim)
+    return [ss[0].strip(), ss[1].strip()]
+
+
+def determineCoordsFromImage(imageFileName):
+    configFolder = pplSetup["PMLIB"]
+    if configFolder == None:
+        configFolder = "$HOME/.pmlib"
+    SOLVE_ARGS = "-O --config " + configFolder + "/astrometry.cfg --use-sextractor --sextractor-path sextractor -r -y -p"
+
+    # /usr/local/astrometry/bin/solve-field $SOLVE_ARGS -D $2 -N $AST_FILE $f
+
+    os.makedirs("temp", exist_ok = True)
+
+    invoke("cp " + imageFileName + " temp/src.fits")
+
+    astFolder = pplSetup["AST_BIN_FOLDER"]
+    if astFolder == None:
+        astFolder = "/usr/local/astrometry/bin"
+
+    astResult = invoke(astFolder + "/solve-field " + SOLVE_ARGS + " -D temp -N temp/ast.fits temp/src.fits")
+
+    r = astResult.split('\n')
+    sCoords = ["0.0", "0.0"]
+    for line in r:
+        if line.startswith("Field center"):
+            if line.find("RA,Dec") > -1:
+                sCoords = getPair(line.split('(')[2].split(')')[0])
+                printInfo("Image center: %s %s" % (sCoords[0], sCoords[1]))
+            else:
+                sCoordsSexa = getPair(line.split('(')[2].split(')')[0])
+                printInfo("Image center: %s %s" % (sCoordsSexa[0], sCoordsSexa[1]))
+        elif line.startswith("Field size"):
+            sFieldSize = getPair(line.split(':')[1].split('a')[0], 'x')
+            printInfo("Image size: %s' x %s'" % (sFieldSize[0], sFieldSize[1]))
+
+    return sCoords
+
+
+def saveCommand(basePath, cmdName):
+    for j in range(len(argv)):
+        if " " in argv[j]:
+            argv[j] = "\"" + argv[j] + "\""
+    cmd = " ".join(argv)
+    cmd = cmd[cmd.find("ppl-"):]
+
+    path = ""
+    j = basePath.rfind("/")
+    if j != -1:
+        path = basePath[:j+1]
+	
+    f = open(path + cmdName, "w+")
+    f.write(cmd + "\n")
+    f.close()
+
+
+def discoverFolders(baseFolder, seqFolderName):
+    seqFolders = []
+    if baseFolder != None:
+        seqFolders = glob('*' + baseFolder + '*/' + seqFolderName + '*')
+    else:
+        seqFolders = glob(seqFolderName + '*')
+    return seqFolders
+
+
+# end pmbase.
---
+++ pmutil/src/main/python/pplphotometry.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python3
+#
+# PmUtils/pplphotometry
+#
+'''
+Created on Feb 22, 2020
+
+@author: kovi
+'''
+from getopt import getopt, GetoptError
+from sys import argv
+from datetime import datetime
+from glob import glob
+from os import getcwd, mkdir
+from os.path import isdir, exists, basename
+
+from pmbase import printError, printWarning, printInfo, saveCommand, loadPplSetup, invoke, Blue, Color_Off, BGreen, discoverFolders
+
+
+
+class Pipeline:
+
+    opt = {}                 # command line options
+    pplSetup = {}            # PPL setup from ppl-setup config file
+
+    AST_CFG = ''             # astrometry.net config file
+    SEX_CFG = ''             # sextractor config file
+    SOLVE_ARGS = ''          # astrometry.net command line arguments
+
+    def __init__(self, opt):
+        self.opt = opt
+
+    def photometry(self, seqFolder, photFolder, refcatFileName, color):
+    
+        # Names of the sequence/combined files:
+        SEQLIST = glob(seqFolder + '/' + self.pplSetup['SEQ_FILE_PREFIX'] + '*-' + color + '.fits')
+        if len(SEQLIST) == 0:
+            SEQLIST = glob(seqFolder + '/Combined-' + color + '.fits')
+            if len(SEQLIST) == 0:
+              printWarning("No files for photometry in folder %s" % (seqFolder))
+              return False
+
+        for f in SEQLIST:
+
+            print("photometry of %s" % (f))
+
+            AST_FILE = photFolder + '/' + basename(f).replace('.fits', '.ast.fits')
+            PMCAT_FILE = photFolder + '/' + basename(f).replace('.fits', '.cat')
+
+            printInfo("Make astrometry for %s" % (f))
+            invoke("%s/solve-field %s -D %s -N %s %s" % (self.pplSetup['AST_BIN_FOLDER'], self.SOLVE_ARGS, photFolder, AST_FILE, f))
+
+            printInfo("Make photometry for %s" % (f))
+            invoke("sextractor %s -c %s -CATALOG_NAME %s -CATALOG_TYPE ASCII_HEAD" % (AST_FILE, self.SEX_CFG, PMCAT_FILE))
+
+            PMCAT_FILE_FLT = PMCAT_FILE + ".cat"
+            printInfo("Filtering result catalog to %s" % (PMCAT_FILE_FLT))
+            # TODO: call directly
+            invoke("pmfilter -c %s -r %s -o %s %s" % (color, refcatFileName, PMCAT_FILE_FLT, PMCAT_FILE))
+
+            PM_FILE = PMCAT_FILE_FLT + '.pm'
+            printInfo("Calculate real magnitudes to %s" % (PM_FILE))
+            # TODO: call directly
+            invoke("pmphot -c %s -o %s %s" % (color, PM_FILE, PMCAT_FILE_FLT))    
+
+        return True
+
+
+    def process_photometry(self, seqFolder, photFolder, title):
+
+        printInfo("%s: Make photometry on sequence file(s)." % (title))
+
+        # Create the photometry dir, if not exists
+        if not exists(photFolder):
+            mkdir(photFolder)
+
+        # Photometry reference file:
+        sf = seqFolder
+        basedir = sf.replace(self.pplSetup['SEQ_FOLDER_NAME'], '')
+        PMREFS = glob(basedir + '/*.cat')
+        if len(PMREFS) > 0 and exists(PMREFS[0]):
+            PMREF = PMREFS[0]
+        else:
+            printError("No reference catalog file (.cat) in folder %s" % (basedir))
+            printInfo("Use ppl-refcat command to create reference catalog for the object.")
+            return False    
+
+        for color in self.opt['color']:
+            self.photometry(seqFolder, photFolder, PMREF, color)
+
+        # TODO: cleanup - delete light FITS files
+
+        return True
+
+
+    def execute(self):
+
+        ##########################
+        # step 0. setup photometry
+        ##########################
+        self.pplSetup = loadPplSetup()
+
+        seqFolders = discoverFolders(self.opt['baseFolder'], self.pplSetup['SEQ_FOLDER_NAME'])
+        if len(seqFolders) == 0:
+            printError('No sequence folder found in base folder %s' % (self.opt['baseFolder']))
+            exit(1)
+        print("Sequence folders discovered:", seqFolders)
+        
+        ### Common arguments (saturation level, image section & trimming, etc.):
+        self.AST_CFG = self.pplSetup['CONFIG_FOLDER'] + "/astrometry.cfg"
+        self.SEX_CFG = self.pplSetup['CONFIG_FOLDER'] + "/sex.cfg"
+        #SOLVE_ARGS="-O --config $AST_CFG --use-sextractor --sextractor-path sextractor -i ${PHOTDIR}/scamp.cat -n ${PHOTDIR}/scamp.cfg -j ${PHOTDIR}/scamp.ref -r -y -p"
+        self.SOLVE_ARGS = "-O --config %s --use-sextractor --sextractor-path sextractor -r -y -p" % (self.AST_CFG)
+
+
+        ####################################
+        # step 1. do photometry for all file
+        ####################################
+
+        PMERROR = False
+        for sf in seqFolders:
+
+            pf = sf.replace(self.pplSetup['SEQ_FOLDER_NAME'], self.pplSetup['PHOT_FOLDER_NAME'])
+    
+            success = self.process_photometry(sf, pf, "PHOTOMETRY")
+            if not success:
+                PMERROR = True
+                break	
+  
+
+        ########################################
+        # step 2. create report from all results
+        ########################################
+
+        if not PMERROR:
+            if self.opt['baseFolder'] != None:
+                PM_FILES = glob('*' + self.opt['baseFolder'] + '*/' + self.pplSetup['PHOT_FOLDER_NAME'] + '/*.pm')
+                BASE_FOLDERS = glob('*' + self.opt['baseFolder'] + '*')
+                REPORT_FOLDER=BASE_FOLDERS[0]
+            else:
+                PM_FILES = glob(self.pplSetup['PHOT_FOLDER_NAME'] + '/*.pm')
+                REPORT_FOLDER = getcwd()
+
+            printInfo("Create report into %s folder." % (REPORT_FOLDER))
+            # TODO: call directly
+            invoke("pmresult -o %s -n %s %s" % (REPORT_FOLDER, self.opt['nameCode'], ' '.join(PM_FILES)))
+
+        print()
+        print(Blue + "Photometry is ready." + Color_Off)
+
+
+class MainApp:
+    
+    opt = {
+        'color' : ['Gi'],    # photometry band, mandatory
+        'nameCode' : None,   # observer code for the AAVSO report, mandatory
+        'overwrite': False,  # force to overwrite existing results, optional
+        'files': None,       
+        'baseFolder': None,  # base folder, optional
+        }
+
+    availableBands = ['gi', 'g', 'bi', 'b', 'ri', 'r', 'all']
+
+
+    def __init__(self, argv):
+        self.argv = argv
+        pass
+
+    def printTitle(self):
+        print()
+        print(BGreen + "pplphotometry, version 1.1.0 " + Color_Off)
+        print(Blue + "Make photometry on calibrated FITS images." + Color_Off)
+        print()
+
+
+    def usage(self):
+        print("Usage: pplphotometry [OPTIONS]... [BASE_FOLDER]")
+        print("Make photometry on calibrated FITS images.")
+        print()
+        print("Mandatory arguments to long options are mandatory for short options too.")
+        print("  -c,  --color arg        set filter(s), arg is the color code, default color is 'Gi', for available color codes see below")
+        print("  -n,  --name nameCode    set observer code for the AAVSO report")
+        print("  -h,  --help             print this page")
+        print("  -w,  --overwrite        force to overwrite existing results")
+        print()
+        print("Available filter color codes are:")
+        print("  Gi | G | gi | g         green channel")
+        print("  Bi | B | bi | b         blue channel")
+        print("  Ri | R | ri | r         red channel")
+        print("  all | ALL | All         all channels, results 3 separate frame")
+        print()
+
+
+    def processCommands(self):
+        try:
+            optlist, args = getopt (self.argv[1:], "c:n:wh", ['--color', '--name', '--help', '--overwrite'])
+        except GetoptError:
+            printError('Invalid command line options.')
+            return
+
+        for o, a in optlist:
+            if a[:1] == ':':
+                a = a[1:]
+            elif o == '-c':
+                color = a.lower()
+                if not color in self.availableBands:
+                    printError('Invalid color: %s, use on of these: Gi, g, Bi, b, Ri, r, all' % (a))
+                    exit(1)
+                if color == 'all':
+                    self.opt['color'] = ['Ri', 'Gi', 'Bi']
+                else:
+                    self.opt['color'] = [a]
+            elif o == '-n':
+                self.opt['nameCode'] = a.upper()
+            elif o == '-w':
+                self.opt['makestd'] = True
+            elif o == '-h':
+                self.usage()
+                exit(0)
+
+        if len(args) > 0:
+            self.opt['baseFolder'] = args[0]
+            if args[0].endswith('/'):
+                self.opt['baseFolder'] = args[0][:-1]
+
+        if self.opt['nameCode'] == None:
+            printWarning('No observer code was given. Use \'XYZ\' instead.')
+            self.opt['nameCode'] = 'XYZ'
+    
+
+    def run(self):
+        self.printTitle()
+        self.processCommands()
+        saveCommand(self.opt['baseFolder'], 'photometry')
+
+        start = datetime.now()
+
+        ppl = Pipeline(self.opt)
+        ppl.execute()
+
+        exectime = (datetime.now() - start).total_seconds()
+        print("%sexecution time was %d seconds.%s" % (Blue, exectime, Color_Off))
+
+
+
+if __name__ == '__main__':
+
+    app = MainApp(argv)
+    app.run()
+
+# end __main__
