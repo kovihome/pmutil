90e7e71389389097e3ad76b41497de9ab75ee619
István Kovács <kovihome86@gmail.com>
2025-06-02 20:25:13 +0200

    Debayer images if BAYERPAT FITS header exists

--- pmutil/src/main/python/pmfits.py
+++ pmutil/src/main/python/pmfits.py
@@ -7,6 +7,7 @@ Created on Jun 24, 2024
 @author: kovi
 @description: FITS manipulation
 """
+from getopt import GetoptError, getopt
 from sys import argv
 from astropy.io import fits
 
@@ -16,42 +17,123 @@ class FitsSeparator:
         self.overwrite = opt["overwrite"]
 
     def saveChannelFits(self, channel_data, original_header, channel, base_filename):
-        new_header = original_header.copy()
-        new_header['FILTER'] = channel
-        hdu = fits.PrimaryHDU(data=channel_data, header=new_header)
-        hdu.writeto(f'{base_filename}-{channel}.fits', overwrite=True)
-        pass
+        if channel in self.colors:
+            new_header = original_header.copy()
+            new_header['FILTER'] = channel
+            if 'BAYERPAT' in new_header:
+                bayer_pattern = new_header['BAYERPAT']
+                del new_header['BAYERPAT']
+                new_header['HISTORY'] = f"Debayering with Bayer pattern {bayer_pattern}"
+            hdu = fits.PrimaryHDU(data=channel_data, header=new_header)
+            hdu.writeto(f'{base_filename}-{channel}.fits', overwrite=True)
 
     def separate(self, fits_filename:str, output_path:str):
+        # open FITS file
+        with fits.open(fits_filename) as hdul:
+            new_base_filename = f"{output_path}/{fits_filename[:fits_filename.rfind('.')]}"
+            # Get header and image data
+            data = hdul[0].data  # vagy hdul[ext].data, ha nem az első HDU-ban van
+            original_header = hdul[0].header
+            naxis = int(original_header['NAXIS'])
+            if naxis == 3:
+                self.separate3D(data, original_header, new_base_filename)
+            else:
+                self.debayer(data, original_header, new_base_filename)
 
-        # FITS file megnyitása
-        hdul = fits.open(fits_filename)
-        original_header = hdul[0].header
-        data = hdul[0].data  # vagy hdul[ext].data, ha nem az első HDU-ban van
+    @staticmethod
+    def separate_channel(data, bayer_pattern, color, second = False):
+        if not bayer_pattern:
+            return data
+        ci = bayer_pattern.index(color) if not second else bayer_pattern.rindex(color)
+        xpi = ci // 2
+        ypi = ci % 2
+        return data[xpi::2, ypi::2]
 
-        # Feltételezve, hogy a data shape-je (3, Y, X) - azaz 3 színcsatorna
-        # Ellenőrizzük:
-        print(data.shape)  # Például: (3, 1024, 1024)
-        new_base_filename = f"{output_path}/{fits_filename[:fits_filename.rfind('.')]}"
+    def separate3D(self, data, original_header, new_base_filename:str):
+        bayer_pattern = original_header['BAYERPAT'] if 'BAYERPAT' in original_header else None
 
-        for n, channel in enumerate(['Bi', 'Gi', 'Ri']):
-            self.saveChannelFits(data[n, :, :], original_header, channel, new_base_filename)
+        if 'Bi' in self.colors:
+            B = self.separate_channel(data[0, :, :], bayer_pattern, 'B')
+            self.saveChannelFits(B, original_header, 'Bi', new_base_filename)
 
-        hdul.close()
+        if 'Gi' in self.colors:
+            if bayer_pattern:
+                G1 = self.separate_channel(data[1, :, :], bayer_pattern, 'G')
+                G2 = self.separate_channel(data[1, :, :], bayer_pattern, 'G', True)
+                G = (G1 + G2) // 2
+            else:
+                G =  data[1, :, :]
+            self.saveChannelFits(G, original_header, 'Gi', new_base_filename)
+
+        if 'Ri' in self.colors:
+            R = self.separate_channel(data[2, :, :], bayer_pattern, 'R')
+            self.saveChannelFits(R, original_header, 'Ri', new_base_filename)
+
+
+    def debayer(self, data, original_header, new_base_filename:str):
+
+        # Separate color channels
+        # ch = [data[0::2, 0::2], data[0::2, 1::2], data[1::2, 0::2], data[1::2, 1::2]]
+
+        # Apply Bayer pattern to resolve channel colors
+        bayer_pattern = original_header['BAYERPAT'] if 'BAYERPAT' in original_header else None
+        # B = ch[bayer_pattern.index('B')]
+        # G1 = ch[bayer_pattern.index('G')]
+        # G2 = ch[bayer_pattern.rindex('G')]
+        # R = ch[bayer_pattern.index('R')]
+        if 'Bi' in self.colors:
+            B = self.separate_channel(data, bayer_pattern, 'B')
+            self.saveChannelFits(B, original_header, 'Bi', new_base_filename)
+
+        if 'Gi' in self.colors:
+            G1 = self.separate_channel(data, bayer_pattern, 'G')
+            G2 = self.separate_channel(data, bayer_pattern, 'G', True)
+            G = (G1 + G2) // 2
+            self.saveChannelFits(G, original_header, 'Gi', new_base_filename)
+
+        if 'Ri' in self.colors:
+            R = self.separate_channel(data, bayer_pattern, 'R')
+            self.saveChannelFits(R, original_header, 'Ri', new_base_filename)
 
 
 if __name__ == '__main__':
-    print("pmfits - separate 3D FITS into 3 color channels")
+    print("pmfits - separate 3D or 2D Bayered FITS into 3 color channels")
     print()
 
-    filename = argv[1]
-    folder = argv[2] if len(argv) > 2 else "."
-    if folder.endswith('/'):
-        folder = folder.rstrip('/')
     opt = {
         'color': ['Ri', 'Gi', 'Bi'],
         'overwrite': True
     }
+
+    try:
+        optlist, args = getopt(argv[1:], "c:h", ['color=', 'help'])
+    except GetoptError:
+        print('Invalid command line options')
+        exit(0)
+
+    for o, a in optlist:
+        if a[:1] == ':':
+            a = a[1:]
+        elif o == '-c' or o == '--color':
+            if a in ['Gi', 'Bi', 'Ri']:
+                opt['color'] = [a]
+            else:
+                print(f"Invalid color code {a}. Available color codes are: Gi, Bi, Ri")
+        elif o == '-h' or o == '--help':
+            print("Usage: pmfits [OPTIONS]... [FITS_FILE] [DESTINATION_FOLDER]")
+            print()
+            print("Mandatory arguments to long options are mandatory for short options too.")
+            print("  -c,  --color arg    use selected color for all channel ; it results monochrome image")
+            print("  -h,  --help         print this page")
+            print()
+            exit(0)
+
+    print(args)
+    filename = args[0]
+    folder = args[1] if len(args) > 1 else "."
+    if folder.endswith('/'):
+        folder = folder.rstrip('/')
+
     separator = FitsSeparator(opt)
     separator.separate(filename, folder)
 
